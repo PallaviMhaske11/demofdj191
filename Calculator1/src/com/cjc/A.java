4eba98491f3332c671b8f39820b4085fc6a280c0
PallaviMhaske11 <pallavimhaske2530@gmail.com>
2025-09-21 22:05:36 +0530

    add Calculator1 project by TL

---
+++ Calculator1/src/com/cjc/A.java
@@ -0,0 +1,20 @@
+package com.cjc;
+
+public class A 
+{
+  
+  public void substraction()
+  {
+	  int x=50;
+	  int y=10;
+	  int z=x-y;
+	  
+	  System.out.println(z);
+  }
+  public static void main(String[] args) 
+  {
+	  A a = new A();
+	  a.substraction();
+	  
+   }
+}
